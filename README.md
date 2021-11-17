bcf65e146d24a10e42277934de7d7e1959f6419b
Aris <masarispati@gmail.com>
2021-11-17 17:07:23 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Postman API Counter
+
+### How to use
+- Download `postman.py`
+- Run this to count the API
+```bash
+python postman.py ~/path/to/postman_collection.json
+```
+- To count spesific folder (1 depth only)
+```bash
+python postman.py ~/path/to/postman_collection.json FolderName
+```
+- Help menu
+```bash
+python postman.py -h
+```
---
+++ postman.py
@@ -0,0 +1,45 @@
+import argparse
+import sys
+import json
+
+parser = argparse.ArgumentParser(description="Postman API request counter")
+parser.add_argument('file', help="JSON file path")
+parser.add_argument('folder', help="Root folder to be count", nargs='?', default=None)
+args = parser.parse_args()
+
+requestCount = 0
+
+def main(jsonFile, rootFolder):
+    file = open(jsonFile)
+    data = json.load(file)
+    node = data['item']
+
+    if (rootFolder != None):
+        index = findIndex(node, rootFolder)
+        
+        if index != None:
+            node = node[index]['item']
+        else:
+            print("{} folder can't be find".format(rootFolder))
+            sys.exit(0)
+    
+    countNode(node)
+
+    print("Total API: {}".format(requestCount))
+
+def findIndex(nodes, search):
+    for index, node in enumerate(nodes):
+        if node['name'] == search:
+            return index
+
+def countNode(nodes):
+    global requestCount
+
+    for node in nodes:
+        if node.has_key('request'):
+            requestCount += 1
+        else:
+            countNode(node['item'])
+
+if __name__ == "__main__":
+    main(args.file, args.folder)
