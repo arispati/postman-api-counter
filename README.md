11f1e345bd522e9ca0ca09b84d6601cd5773909a
Aris <masarispati@gmail.com>
2021-11-18 11:11:48 +0700

    depth root folder support

--- README.md
+++ README.md
@@ -5,10 +5,34 @@
 - Run this to count the API
 ```bash
 python postman.py ~/path/to/postman_collection.json
+# Total API: 57
 ```
-- To count spesific folder (1 depth only)
+- Show available folder
 ```bash
-python postman.py ~/path/to/postman_collection.json FolderName
+python postman.py ~/path/to/postman_collection.json -l
+# User
+# Business
+# Order
+# Product
+
+python postman.py ~/path/to/postman_collection.json User -l
+# Auth
+# General
+
+python postman.py ~/path/to/postman_collection.json User.General -l
+# OTP
+# Message
+```
+- To count spesific folder
+```bash
+python postman.py ~/path/to/postman_collection.json User
+# Total API: 24
+
+python postman.py ~/path/to/postman_collection.json User.General
+# Total API: 10
+
+python postman.py ~/path/to/postman_collection.json User.General.OTP
+# Total API: 2
 ```
 - Help menu
 ```bash
--- postman.py
+++ postman.py
@@ -5,41 +5,58 @@ import json
 parser = argparse.ArgumentParser(description="Postman API request counter")
 parser.add_argument('file', help="JSON file path")
 parser.add_argument('folder', help="Root folder to be count", nargs='?', default=None)
+parser.add_argument('-l', '--list', help="Show folder list", action="store_true")
 args = parser.parse_args()
-
 apiCount = 0
 
 def main(jsonFile, rootFolder):
-    file = open(jsonFile)
-    data = json.load(file)
-    node = data['item']
+  jsonData = openFile(jsonFile)
+  rootNodes = getRootNodes(jsonData, rootFolder)
+  if args.list:
+    showList(rootNodes)
+  else:
+    counter(rootNodes)
 
-    if (rootFolder != None):
-        index = findIndex(node, rootFolder)
-        
-        if index != None:
-            node = node[index]['item']
-        else:
-            print("Root folder '{}' can not be found".format(rootFolder))
-            sys.exit(0)
-    
-    countTheAPI(node)
+def counter(nodes):
+  countTheAPI(nodes)
+  print("Total API: {}".format(apiCount))
 
-    print("Total API: {}".format(apiCount))
+def showList(nodes):
+  for node in nodes:
+    print(node['name'])
+
+def getRootNodes(nodes, rootFolder):
+  if (rootFolder == None):
+    return nodes
+  else:
+    paths = rootFolder.split('.')
+    rootNodes = nodes
+    for path in paths:
+      index = findIndex(rootNodes, path)
+      if index != None:
+        rootNodes = rootNodes[index]['item']
+      else:
+        print("Root folder '{}' can not be found".format(rootFolder))
+        sys.exit(0)
+    return rootNodes
+
+def openFile(jsonFile):
+  file = open(jsonFile)
+  data = json.load(file)
+  return data['item']
 
 def findIndex(nodes, search):
-    for index, node in enumerate(nodes):
-        if node['name'] == search:
-            return index
+  for index, node in enumerate(nodes):
+    if node['name'] == search:
+      return index
 
 def countTheAPI(nodes):
-    global apiCount
-
-    for node in nodes:
-        if node.has_key('request'):
-            apiCount += 1
-        else:
-            countTheAPI(node['item'])
+  global apiCount
+  for node in nodes:
+    if node.has_key('request'):
+      apiCount += 1
+    else:
+      countTheAPI(node['item'])
 
 if __name__ == "__main__":
-    main(args.file, args.folder)
+  main(args.file, args.folder)
